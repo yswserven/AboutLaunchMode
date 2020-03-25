72a949aa8c016adc5538fdf982908fcedccd1192
yswserven <yswserven@gmail.com>
2020-03-25 15:57:10 +0800

    '添加一个RecyclerViewActivity'

---
+++ app/src/main/java/com/custom/launchmode/ui/RecyclerViewActivity.java
@@ -0,0 +1,16 @@
+package com.custom.launchmode.ui;
+
+import android.os.Bundle;
+
+import com.custom.launchmode.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
